e355f237a403e07e7edba3d5c3eb7792e0a1a59b
Ronak J Vanpariya <26689210+vanpariyar@users.noreply.github.com>
2025-03-11 16:59:26 +0000

    Fix: Added Plugin

---
+++ sensie-google-login-with-rtcamp-plugin/sensie-customise.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: Sensie Google Login with rtCamp
+Plugin URI: https://github.com/sahajananddigital/wordpress-plugins/sensie-google-login-with-rtcamp-plugin
+Requires at least: 5.2
+Tested up to: 5.8
+Requires PHP: 7.0
+Requires WordPress: 5.2
+Requires Plugins: sensei-lms, login-with-google
+Description: A plugin to customize Google login with rtCamp for Sensie.
+Version: 1.0.0
+Author: Sahajanand Digital
+Author URI: https://sahajananddigital.in
+License: GPL2
+*/
+?>
+
+<?php
+// Add custom code here
+add_action('sensei_login_form_after', 'sensie_google_login_with_rtcamp');
+function sensie_google_login_with_rtcamp() {
+    echo do_shortcode('[google_login button_text="Google Login" force_display="yes" /]');
+}
